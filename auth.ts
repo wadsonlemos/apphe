1614292c4f1cd4206a22bc9818dbb8f826b0959f
Wadson Lemos <wadsonlemos@gmail.com>
2026-02-12 01:31:52 -0300

    fix: restore missing bcrypt import

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
+import bcrypt from 'bcryptjs';
 import { prisma } from '@/app/lib/db';
 
 async function getUser(username: string) {
