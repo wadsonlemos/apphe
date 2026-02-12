5a115635e09a45778f1e0842ba8b6eb598b6f690
Wadson Lemos <wadsonlemos@gmail.com>
2026-02-12 00:24:50 -0300

    fix: disable prisma adapter to resolve constructor error

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from '@prisma/config';
-import { PrismaLibSQL } from '@prisma/adapter-libsql';
-import { createClient } from '@libsql/client';
-
-const client = createClient({ url: 'file:app.db' });
-const adapter = new PrismaLibSQL(client);
 
 export default defineConfig({
     schema: 'prisma/schema.prisma',
-    adapter,
+    datasource: {
+        provider: 'sqlite',
+        url: "file:app.db",
+    }
 });
